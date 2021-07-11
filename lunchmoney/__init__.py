918876786198ce299bf636a0426299a3827ecbc3
Christofon <kremsnerchristopher@gmail.com>
2021-07-12 00:11:26 +0200

    init files and auth

---
+++ lunchmoney/__init__.py
@@ -0,0 +1,24 @@
+import os
+import requests
+
+# TODO maybe only for testing needed
+# LUNCHMONEY_API_KEY = os.environ.get('LUNCHMONEY_API_KEY', None)
+from dotenv import load_dotenv
+load_dotenv()
+LUNCHMONEY_API_KEY = os.getenv('LUNCHMONEY_API_KEY')
+
+
+class APIKeyMissingError(Exception):
+    pass
+
+
+if LUNCHMONEY_API_KEY is None:
+    raise APIKeyMissingError(
+        'All functionality require an API key. Visit "https://my.lunchmoney.app/developers" to get one.'
+    )
+
+session = requests.Session()
+session.params = {}
+session.params['access_token'] = LUNCHMONEY_API_KEY
+
+from .categories import Categories
