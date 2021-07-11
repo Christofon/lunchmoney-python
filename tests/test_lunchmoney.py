8698b19dc5c2c54d7b199a310ee13ff499ced2d2
Christofon <kremsnerchristopher@gmail.com>
2021-07-12 00:34:01 +0200

    categories proper test

--- tests/test_lunchmoney.py
+++ tests/test_lunchmoney.py
@@ -2,10 +2,18 @@ from pytest import fixture
 # to only define a subset of the response to test on
 from lunchmoney import Categories
 
-def test_get_all_categories():
+
+@fixture
+def categories_keys():
+    return ['id', 'exclude_from_budget', 'is_income']
+
+
+def test_get_all_categories(categories_keys):
     """Tests if a list of all categories associated with the user's account is returned"""
 
     categories_instance = Categories()
     response = categories_instance.get_all_categories()
 
     assert isinstance(response, dict)
+    assert set(categories_keys).issubset(
+        dict(response['categories'][0]).keys())
